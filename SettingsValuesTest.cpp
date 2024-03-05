56fb1e5b0758ba752b9325dca76f7285d35118d6
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-03-05 18:22:36 +0200

    tested SettingsValues

--- SettingsValuesTest.cpp
+++ SettingsValuesTest.cpp
@@ -3,8 +3,79 @@
 
 namespace SettingsValuesTest
 {
-	TEST(Name1, Name1)
+	TEST(DefaultValues, DefaultValues)
 	{
-		EXPECT_EQ(1, 2);
+		SettingsValues settings;
+
+		EXPECT_EQ(settings.getLanguage(), 0);
+		EXPECT_EQ(settings.getSkin(), 0);
+		EXPECT_EQ(settings.getWidth(), 1920);
+		EXPECT_EQ(settings.getHeight(), 1080);
+		EXPECT_EQ(settings.getSettingTitle(), 573);
+		EXPECT_EQ(settings.getTitle(), 469.5);
+		EXPECT_EQ(settings.getFullscreen(), 0);
+	}
+
+	TEST(SetValues, Language)
+	{
+		SettingsValues settings;
+
+		settings.setLanguage(1);
+
+		EXPECT_EQ(settings.getLanguage(), 1);
+	}
+
+	TEST(SetValues, Skin)
+	{
+		SettingsValues settings;
+
+		settings.setSkin(1);
+
+		EXPECT_EQ(settings.getSkin(), 1);
+	}
+
+	TEST(SetValues, Width)
+	{
+		SettingsValues settings;
+
+		settings.setWidth(1280);
+
+		EXPECT_EQ(settings.getWidth(), 1280);
+	}
+
+	TEST(SetValues, Height)
+	{
+		SettingsValues settings;
+
+		settings.setHeight(970);
+
+		EXPECT_EQ(settings.getHeight(), 970);
+	}
+
+	TEST(SetValues, SettingTitle)
+	{
+		SettingsValues settings;
+
+		settings.setSettingTitle(600);
+
+		EXPECT_EQ(settings.getSettingTitle(), 600);
+	}
+
+	TEST(SetValues, Title)
+	{
+		SettingsValues settings;
+
+		settings.setTitle(600);
+
+		EXPECT_EQ(settings.getTitle(), 600);
+	}
+
+	TEST(SetValues, Fullscreen)
+	{
+		SettingsValues settings;
+
+		settings.setFullscreen(true);
+
+		EXPECT_EQ(settings.getFullscreen(), 1);
 	}
 }
