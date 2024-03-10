4248b65e7548a8e5c72225e328263c896f83d358
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-03-10 11:56:33 +0200

    part tested Game

--- GameTest.cpp
+++ GameTest.cpp
@@ -3,8 +3,35 @@
 
 namespace GameTest
 {
-	TEST(Name5, Name5)
+	TEST(CreateWindow, CreateWindow)
 	{
-		EXPECT_EQ(1, 1);
+		Game game;
+		game.createWindow();
+	}
+
+	TEST(Mainloop, Mainloop)
+	{
+		Game game;
+		game.createWindow();
+		//game.mainloop(); // Failed to load font "source/fontes/Gilroy-Heavy.woff" (failed to create the font face) 
+	}
+
+	TEST(WidthAndHeight, DefaultValues)
+	{
+		Game game;
+
+		EXPECT_EQ(game.getWidth(), sf::VideoMode::getDesktopMode().width);
+		EXPECT_EQ(game.getHeight(), sf::VideoMode::getDesktopMode().height);
+	}
+
+	TEST(WidthAndHeight, SetValues)
+	{
+		Game game;
+
+		game.setWidth(10);
+		game.setHeight(10);
+
+		EXPECT_EQ(game.getWidth(), 10);
+		EXPECT_EQ(game.getHeight(), 10);
 	}
 }
