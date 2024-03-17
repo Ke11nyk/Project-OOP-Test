c60eac18282039fd6d87186868e95f2d0b7457d9
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-03-17 15:15:20 +0200

    - borders i don`t know for what they was

--- PlayerTest.cpp
+++ PlayerTest.cpp
@@ -6,7 +6,7 @@ namespace PlayerTest
 	sf::RenderWindow WWin;
 	std::vector<sf::String> vecTileMap = {"aaaa", "aaaa", "aaaa"};
 
-	TEST(Name2, Name2)
+	TEST(CreatePlayer, CreatePlayer)
 	{
 		Player stick = Player(WWin, vecTileMap, "D:/Uni materials/2 course/OOP/Project OOP/source/images/figure.png", 3);
 	}
