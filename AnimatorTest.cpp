1cd9b3dbd4364e99cf83faeed0770948655b56f9
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-03-17 12:26:42 +0200

    tested Animator

--- AnimatorTest.cpp
+++ AnimatorTest.cpp
@@ -8,10 +8,61 @@ namespace AnimatorTest
 		sf::Sprite SStickSprite; ///< Sprite of the player
 		Animator StickAnim = Animator(SStickSprite);  ///< Animation of the player
 		sf::String StrTexture = "D:/Uni materials/2 course/OOP/Project OOP/source/images/figure.png"; ///< Texture of the player
-		float fTime = 0.5;
 		sf::Vector2i iSpriteSize = sf::Vector2i(54, 60);
  
-		auto& idleForwardStatic = StickAnim.CreateAnimation("idleForwardStatic", StrTexture, sf::seconds(fTime), true);
+		auto& idleForwardStatic = StickAnim.CreateAnimation("idleForwardStatic", StrTexture, sf::seconds(0.5), true);
 		idleForwardStatic.AddFrames(sf::Vector2i(0, 0), iSpriteSize, 1, 1);
+	}
+
+	TEST(SwitchAnimation, SwitchAnimation)
+	{
+		sf::Sprite SStickSprite; ///< Sprite of the player
+		Animator StickAnim = Animator(SStickSprite);  ///< Animation of the player
+		sf::String StrTexture = "D:/Uni materials/2 course/OOP/Project OOP/source/images/figure.png"; ///< Texture of the player
+
+		StickAnim.CreateAnimation("idleForwardStatic", StrTexture, sf::seconds(0.5), true);
+
+		ASSERT_EQ(StickAnim.SwitchAnimation("idleBack"), false);
+		ASSERT_EQ(StickAnim.SwitchAnimation("idleForwardStatic"), true);
+	}
+
+	TEST(GetCurrentAnimationName, GetCurrentAnimationName)
+	{
+		sf::Sprite SStickSprite; ///< Sprite of the player
+		Animator StickAnim = Animator(SStickSprite);  ///< Animation of the player
+		sf::String StrTexture = "D:/Uni materials/2 course/OOP/Project OOP/source/images/figure.png"; ///< Texture of the player
+
+		ASSERT_EQ(StickAnim.GetCurrentAnimationName(), "");
+
+		StickAnim.CreateAnimation("idleForwardStatic", StrTexture, sf::seconds(0.5), true);
+		StickAnim.CreateAnimation("idleForward", StrTexture, sf::seconds(0.5), true);
+
+		ASSERT_EQ(StickAnim.GetCurrentAnimationName(), "idleForwardStatic");
+
+		StickAnim.SwitchAnimation("idleForward");
+
+		ASSERT_EQ(StickAnim.GetCurrentAnimationName(), "idleForward");
+	}
+
+	TEST(UpdateAndRestart, UpdateAndRestart)
+	{
+		sf::Sprite SStickSprite; ///< Sprite of the player
+		Animator StickAnim = Animator(SStickSprite);  ///< Animation of the player
+		sf::String StrTexture = "D:/Uni materials/2 course/OOP/Project OOP/source/images/figure.png"; ///< Texture of the player
+		float fTime = 0.5;
+		sf::Vector2i iSpriteSize = sf::Vector2i(54, 60);
+
+		auto& idleForwardStatic = StickAnim.CreateAnimation("idleForwardStatic", StrTexture, sf::seconds(fTime), false);
+		idleForwardStatic.AddFrames(sf::Vector2i(0, 0), iSpriteSize, 1, 1);
+
+		ASSERT_EQ(StickAnim.getEndAnim(), false);
+
+		StickAnim.Update(sf::seconds(fTime));
+
+		ASSERT_EQ(StickAnim.getEndAnim(), true);
+
+		StickAnim.Restart();
+		
+		ASSERT_EQ(StickAnim.getEndAnim(), false);
 	}
 }
